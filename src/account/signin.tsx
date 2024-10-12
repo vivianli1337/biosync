2da8c1fa4d6562f84bf9f73ed5000f18b4d5c31e
vivianli1337 <vivianli1337@gmail.com>
2024-10-12 13:53:47 -0400

    account

---
+++ src/account/signin.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+export default function Signin() {
+    return (
+        <div id="signin-screen" className="d-flex justify-content-center align-items-center vh-100 bg-light">
+            <div className="signin-box bg-white p-5 round-r shadow-lg text-center">
+                <h1 className="mb-4">LOG IN</h1>
+                <form className="d-flex flex-column align-items-center w-100">
+                    <div className="mb-3 w-100">
+                        <input
+                            id="username"
+                            className="form-control"
+                            placeholder="username"
+                        />
+                    </div>
+                    <div className="mb-3 w-100">
+                        <input
+                            id="password"
+                            className="form-control"
+                            placeholder="password"
+                            type="password"
+                        />
+                    </div>
+                    <button className="btn btn-secondary w-100 mb-3">
+                        LOG IN
+                    </button>
+                </form>
+                <div className="signup-link text-end">
+                    <Link id="signup-link" to="/account/signup">
+                        sign up
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/account/signup.tsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+import './index.css';
+
+export default function Signup() {
+  return (
+    <div id="wd-signup-screen" className="d-flex justify-content-center align-items-center vh-100 bg-light">
+      <div className="signup-box bg-white p-5 round-r shadow-lg">
+        <h1 className="mb-4 text-center">SIGN UP</h1>
+        <form className="d-flex flex-column">
+          <div className="mb-3">
+            <input id="wd-username" placeholder="Username" className="form-control custom-input" />
+          </div>
+          <div className="mb-3">
+            <input id="wd-hospital" placeholder="Hospital" className="form-control custom-input" />
+          </div>
+          <div className="mb-3">
+            <input id="wd-branch" placeholder="Branch" className="form-control custom-input" />
+          </div>
+          <div className="mb-3">
+            <input id="wd-email" placeholder="Email" type="email" className="form-control custom-input" />
+          </div>
+          <div className="mb-3">
+            <input id="wd-address" placeholder="Address" className="form-control custom-input" />
+          </div>
+          <div className="row g-2 mb-3">
+            <div className="col">
+              <input id="wd-city" placeholder="City" className="form-control custom-input" />
+            </div>
+            <div className="col">
+              <input id="wd-state" placeholder="State" className="form-control custom-input" />
+            </div>
+            <div className="col">
+              <input id="wd-zipcode" placeholder="Zip Code" className="form-control custom-input" />
+            </div>
+          </div>
+          <div className="mb-3">
+            <input id="wd-password" placeholder="Password" type="password" className="form-control custom-input" />
+          </div>
+          <div className="mb-3">
+            <input id="wd-password-verify" placeholder="Verified Password" type="password" className="form-control custom-input" />
+          </div>
+          <button id="wd-signup-btn" className="btn btn-primary w-100 mb-3">
+            Sign Up
+          </button>
+        </form>
+        <div className="text-end">
+          <Link id="wd-signin-link" to="/Account/signin">Sign In</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
