e34f60fb02ed929c9e043fd54f84d0b613b30fb2
vivianli1337 <vivianli1337@gmail.com>
2024-10-12 22:34:57 -0400

    add

--- src/content/Summary/urgent.jsx
+++ src/content/Summary/urgent.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom'; // Make sure to import Link
 import './index.css';
 
 export default function UrgentMessage() {
     return (
         <div className="alert alert-danger d-flex justify-content-between align-items-center" role="alert">
             <span>ALERT: Hospital X is in URGENT need of 5 Type O Blood bags</span>
-            <button className="btn btn-success btn-assist">assist</button>
+            <Link to="/content/Inventories/sendinventory">
+                <button className="btn btn-success btn-assist">assist</button>
+            </Link>
         </div>
     );
 }
